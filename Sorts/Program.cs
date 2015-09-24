f3e21008a9e55193ca884023fcbb264daba2ebd4
xuzz <18895913@qq.com>
2015-09-24 09:27:46 +0800

    改进快排 Signed-off-by: xuzz <18895913@qq.com>

--- Sorts/Program.cs
+++ Sorts/Program.cs
@@ -11,11 +11,11 @@ namespace Sorts
         static readonly int[] x = { 6, 1, 2, 7, 9, 3, 4, 5, 10, 8 };
         static void Main(string[] args)
         {
-            //QuickSort.Sort(x, 0, x.Length - 1);
+            QuickSort.Sort(x, 0, x.Length - 1);
 
             //SelectionSort.Sort(x);
 
-            InsertSort.Sort(x);
+            //InsertSort.Sort(x);
 
             for (var i = 0; i < x.Length; i++)
             {
