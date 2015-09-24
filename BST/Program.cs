26dde3b9fcdabed68b58971af108902451626895
xuzz <xuzz@chinasap.cn>
2015-09-24 23:57:49 +0800

    修正快排，添加BST，添加约瑟夫，添加空的哈希项目
    Signed-off-by: xuzz <xuzz@chinasap.cn>

--- BST/Program.cs
+++ BST/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*
+    author : xuzz@2015-09-23
+*/
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +10,9 @@ using System.Threading.Tasks;
 
 /// <summary>
 /// 二叉查找树、二叉搜索树、二叉排序树 说的都是这货！
+/// 
+/// 相对较少的值保存在左节点上，较大的值保存在右节点中
+/// 这使得查找比较方便。
 /// </summary>
 namespace BST
 {
@@ -17,11 +23,37 @@ namespace BST
             int[] datas = { 23, 45, 16, 37, 3, 99, 22 };
 
             var bst = new BST();
-            foreach (var data in datas)
-            {
-                bst.BuildBST(data);
-            }
-            var bstRoot = bst.Root;
+            //foreach (var data in datas)
+            //{
+            //    bst.BuildBST(data);
+            //}
+
+            //var bstRoot = bst.Root;
+            var bstRoot = bst.BuildBST(datas);
+
+            Console.WriteLine("中序遍历：");
+            bst.InOrder(bstRoot);
+
+            Console.WriteLine("\r\n先序遍历：");
+            bst.PreOrder(bstRoot);
+
+            Console.WriteLine("\r\n后序遍历：");
+            bst.PostOrder(bstRoot);
+
+            Console.WriteLine("\r\n最小值：");
+            Console.Write(bst.FindMin());
+
+            Console.WriteLine("\r\n最小值节点：");
+            Console.Write(bst.FindMinNode());
+
+            Console.WriteLine("\r\n最大值：");
+            Console.Write(bst.FindMax());
+
+            Console.WriteLine("\r\n最大值节点：");
+            Console.Write(bst.FindMaxNode());
+
+            Console.WriteLine("\r\n查找指定值：");
+            Console.Write(bst.FindNode(45));
 
             Console.ReadLine();
             
@@ -54,6 +86,11 @@ namespace BST
             }
             return result;
         }
+
+        public override string ToString()
+        {
+            return string.Format("节点的值是:{0}", this.Data);
+        }
     }
 
     public class BST
@@ -62,9 +99,19 @@ namespace BST
         {
             get;
             set;
-        }            
+        }
 
-        public void BuildBST(int data)
+        public Node BuildBST(int[] arrDatas)
+        {
+            foreach (var arrData in arrDatas)
+            {
+                BuildBST(arrData);
+            }
+
+            return this.Root;
+        }
+
+        private void BuildBST(int data)
         {
             var node = new Node() { Data = data, LChild = null, RChild = null };
             if (null == Root)
@@ -73,30 +120,175 @@ namespace BST
                 return;
             }
 
+            //指向当前节点。从根节点开始查找可以插入的位置。
+            //如果需要创建的值小于该节点，则往当前节点的左子树查找。否则往右子树查找。
+            //如果在左子树查找，且左子树的LChlid为空，说明这就是需要插入的位置
+            //右子树也是同样的道理
             var current = Root;
-            Node parent = null;
             while (current != null)
             {
-                parent = current;
                 if (data < current.Data)
                 {
-                    current = current.LChild;
-                    if (null == current)
+                    if (null == current.LChild)
                     {
-                        parent.LChild = node;
+                        current.LChild = node;
                         break;
+                    }
+                    else
+                    {
+                        current = current.LChild;
                     }
                 }
                 else
                 {
-                    current = current.RChild;
-                    if (null == current)
+                    if (null == current.RChild)
                     {
-                        parent.RChild = node;
+                        current.RChild = node;
+                        break;
                     }
+                    else
+                    {
+                        current = current.RChild;
+                    }
+                    
                 }
 
             }
+        }
+
+        /// <summary>
+        /// 中序遍历(先输出根节点，再输出左子树，最后输出右子树）
+        /// 中序遍历输出的顺序总是有序的。
+        /// </summary>
+        public void InOrder(Node node)
+        {
+            if (null != node)
+            {
+
+                InOrder(node.LChild);
+
+                Console.Write(node.Data+",");
+
+                InOrder(node.RChild);    
+                            
+            }
+        }
+
+        /// <summary>
+        /// 先序遍历
+        /// </summary>
+        /// <param name="node"></param>
+        public void PreOrder(Node node)
+        {
+            if (null != node)
+            {
+                Console.Write(node.Data+",");
+
+                PreOrder(node.LChild);
+
+                PreOrder(node.RChild);
+            }
+        }
+
+        /// <summary>
+        /// 后序遍历
+        /// </summary>
+        /// <param name="node"></param>
+        public void PostOrder(Node node)
+        {
+            if (null != node)
+            {
+                PostOrder(node.LChild);
+                PostOrder(node.RChild);
+                Console.Write(node.Data + ",");
+            }
+        }
+
+        /// <summary>
+        /// 查找最小值，最小值总是在左子树的最后一个节点
+        /// </summary>
+        /// <returns></returns>
+        public int FindMin()
+        {
+            var current = Root;
+            while (null != current.LChild)
+            {
+                current = current.LChild;
+            }
+
+            return current.Data;
+        }
+
+        /// <summary>
+        /// 最小值节点
+        /// </summary>
+        /// <returns></returns>
+        public Node FindMinNode()
+        {
+            var current = Root;
+            while (null != current.LChild)
+            {
+                current = current.LChild;
+            }
+            return current;
+        }
+
+        /// <summary>
+        /// 查找最大值。最大值总是在右子树的最后一个节点
+        /// </summary>
+        /// <returns></returns>
+        public int FindMax()
+        {
+            var current = Root;
+            while (null != current.RChild)
+            {
+                current = current.RChild;
+            }
+            return current.Data;
+        }
+
+        /// <summary>
+        /// 寻找最大值所在的节点
+        /// </summary>
+        /// <returns></returns>
+        public Node FindMaxNode()
+        {
+            var current = Root;
+            while (null != current.RChild)
+            {
+                current = current.RChild;
+            }
+            return current;
+        }
+        
+        /// <summary>
+        /// 根据传入的值，查找节点。
+        /// 
+        /// 在二叉树上查找给定值，需要比较该值和当前节点上的值得大小。通过比较，就能确定如果给定值不在当前节点时，就要向左遍历和向右遍历了。
+        /// </summary>
+        /// <param name="iData"></param>
+        /// <returns></returns>
+        public Node FindNode(int iData)
+        {
+            var current = Root;
+            while (null != current)
+            {
+                if (current.Data == iData)
+                {
+                    return current;
+                }
+                //如果当前节点的值比需要查找的小，说明需要查找的节点在当前节点的右边
+                if (current.Data < iData)
+                {
+                    current = current.RChild;
+                }
+                //如果当前节点的值比需要查找的大，说明需要查找的节点在当前节点的左边
+                else if (current.Data > iData)
+                {
+                    current = current.LChild;
+                }
+            }
+            return null;
         }
     }
 }
---
+++ JosephusRing/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 约瑟夫环
+/// </summary>
+namespace JosephusRing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kids = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+            //Josephus.KickoutByMath(kids, 3);
+
+            var linkNode = Josephus.BuildLinkedList(kids);
+            Josephus.KickoutByLink(linkNode);
+            Console.Read();
+        }
+    }
+
+    public class JosephusNode
+    {
+        public bool IsKickout
+        {
+            get; set;
+        }
+        public JosephusNode Next
+        {
+            get;
+            set;
+        }
+
+        public int Index
+        {
+            get;
+            set;
+        }
+ 
+    }
+
+    public class Josephus
+    {
+        public static void KickoutByLink(JosephusNode head)
+        {
+            var pre = head;
+            var curr = head;
+            int count = 1;
+            while (null!= curr)
+            {
+                if (count == 3)
+                {
+                    Console.WriteLine("出圈{0}:", curr.Index);                    
+                    pre.Next = curr.Next;
+                    count = 0;
+                }
+                pre = curr;
+                curr = curr.Next;
+                count++;
+
+                if (pre==curr)
+                {
+                    Console.WriteLine("留下{0}:", curr.Index);
+                    break;
+                }
+            }
+        }
+
+        public static JosephusNode BuildLinkedList(List<int> kids)
+        {
+            var head = new JosephusNode()
+            {
+                Next = null,
+                Index = 0
+            };
+            var tmp = head;
+            head.Next = tmp;
+            for (var i = 0; i < kids.Count(); i++)
+            {
+                if (i == 0)
+                {
+                    head.Index = kids[i];
+                }
+                else
+                {
+                    tmp.Next = new JosephusNode()
+                    {
+                        Next = null,
+                        Index = kids[i]
+                    };
+                    tmp = tmp.Next;
+                }
+            }
+            tmp.Next = head;
+            //var tmp = head;
+            //head.Next = tmp;
+            //kids.ForEach(k =>
+            //{
+            //    tmp.Next= new JosephusNode()
+            //    {
+            //        Next = null,
+            //        Index = k
+            //    };
+            //    tmp = tmp.Next;
+            //});
+            //tmp.Next = head;
+            return head;
+
+            //var list = new JosephusNode()
+            //{
+            //    ID = Guid.NewGuid().ToString(),
+            //    Next = null,
+            //    Index = 0
+            //};
+            //var current = new JosephusNode()
+            //{
+            //    ID = Guid.NewGuid().ToString(),
+            //    Next = null,
+            //    Index = 0
+            //};
+            //list.Next = current;
+            ////for (int i = 0; i < kids.Count(); i++)
+            ////{
+            ////    if (i == 0)
+            ////    {
+            ////        current.Index = kids[i];
+            ////    }
+            ////    else
+            ////    {
+            ////        current.Next = new JosephusNode()
+            ////        {
+            ////            ID = Guid.NewGuid().ToString(),
+            ////            Next = null,
+            ////            Index = kids[i]
+            ////        };
+
+            ////        current = current.Next;
+            ////    }
+            ////}
+            //kids.ForEach(kid =>
+            //{
+            //    current.Next = new JosephusNode()
+            //    {
+            //        ID = Guid.NewGuid().ToString(),
+            //        Next = null,
+            //        Index = kid
+            //    };
+
+            //    current = current.Next;
+            //});
+
+            //current.Next = list;
+
+            //return list;
+        }
+
+        /// <summary>
+        /// 使用数学方法
+        /// </summary>
+        /// <param name="kids">人员集合</param>
+        /// <param name="M">数到第几出列？</param>
+        public static void KickoutByMath(List<int> kids, int M)
+        {
+            var i = M-1 % kids.Count;   
+            //M%len 一定是第一个出列的人.比如总共有17个人，数到3出列，那第一个出列的一定是3（3%17=3）
+            while (kids.Count > 1)
+            {
+                Console.WriteLine("{0} 出列", kids[i]);
+                kids.RemoveAt(i);
+                i = (i+M-1) % kids.Count;
+            }
+            Console.WriteLine("留下{0}", kids[i]);
+        }
+    }
+
+}
