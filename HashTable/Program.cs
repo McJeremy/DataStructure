552dadfdec01b0b47599de0e642e4d86cdd38177
xuzz <xuzz@chinasap.cn>
2015-10-10 21:45:04 +0800

    添加哈希表的说明 Signed-off-by: xuzz <xuzz@chinasap.cn>

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -13,6 +13,9 @@ namespace HashTable
             string[] strNames =new string[99];
             string[] strSomeNames = { "xuzz","Bob","Jack","Stephen","Mayno","Danina","Michael","Mike"};
 
+            //两种求  散列键值的相同点都是处理完字符的ascii码后，与存储数据的数组长度相除求余
+            //不同的是，仅仅是ascii码相加，键值分布不均匀，而且容易冲突
+            //而采用一个素数的方式，键值分布相对集中。
             foreach (var strName in strSomeNames)
             {
                 strNames[BetterHash(strName, strNames)] = strName;
@@ -30,6 +33,7 @@ namespace HashTable
 
         static int SimpleHash(string str, string[] s)
         {
+            //将所有字符的ascii码想加，再除以存储数据的数组长度，然后取余作为存储位置（下标）
             var arrLen = s.GetUpperBound(0);
 
             char[] cs = str.ToCharArray();
@@ -49,12 +53,14 @@ namespace HashTable
 
             char[] cs = str.ToCharArray();
 
+            //采用一个素数来与想加的acsii码相乘，然后再加上当前字符ascii码
             int tot = 0;
             for (var i = 0; i < cs.Length; i++)
             {
                 tot += (37*tot + (int)(cs[i]));
             }
 
+            //再除以存储数据的数组长度，然后取余作为存储位置（下标）
             tot = tot % arrLen;
 
             if (tot < 0)
